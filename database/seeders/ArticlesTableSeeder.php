a6ed229752e655969c25253fbad210f488e85163
Basmasayed2002 <2102395@talnet.nl>
2024-12-05 11:47:36 +0100

    API

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Article;
+
+class ArticlesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // Let's truncate our existing records to start from scratch.
+        Article::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few articles in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Article::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+            ]);
+        }
+    }
+}
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // Let's clear the users table first
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // Let's make sure everyone has the same password and 
+        // let's hash it before the loop, or else our seeder 
+        // will be too slow.
+        $password = Hash::make('toptal');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password,
+        ]);
+
+        // And now let's generate a few dozen users for our app:
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call(ArticlesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+    }
+}
